42429bbbd730cc9a3e84a738d54b701ef6f5871f
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-03-25 23:21:51 +0300

    fix: resolve issue with Menu for Auth

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Logo } from '../components/Logo';
 import NavBar from '../components/NavBar';
 
-export default class Header extends React.Component {
-    render () {
+class Header extends React.Component {
+    render() {
         return (
             <header>
                 <div className="header-wrapper">
                     <div className="header">
                         <Logo />
-                        <NavBar />
+                        { this.props.isAuth.isAuth && <NavBar /> }
                     </div>
                 </div>
             </header>
         );
     }
 }
+
+const mapStateToProps = state => ({
+    isAuth: state.isAuth
+});
+
+export default connect(mapStateToProps, null)(Header);
